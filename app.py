f2ed813fcedfe1976f1f21d6a1608b4a22758d3a
shdk052 <bniatatrimshahar@gmail.com>
2025-05-23 10:07:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,6 @@ def hello():
     return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
